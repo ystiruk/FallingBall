40d74a7cf76b7d710c4d66398e0fbe6304c654fb
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2020-08-08 17:07:56 +0300

    Make rotation smooth

--- source/FallingBall3D/Assets/Scripts/PlaneController.cs
+++ source/FallingBall3D/Assets/Scripts/PlaneController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
@@ -15,13 +16,26 @@ public class PlaneController : MonoBehaviour
         m_PlaneRollData.ZAngleUpdated += M_PlaneRollData_ZAngleUpdated;
     }
 
+    IEnumerator RotateInTime(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(/*transform.eulerAngles + */byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+    }
+
     private void M_PlaneRollData_ZAngleUpdated(int obj)
     {
-        plane.localRotation = Quaternion.Euler(0, 0, obj);
+        //plane.localRotation = Quaternion.Euler(0, 0, obj);
+        StartCoroutine(RotateInTime(Vector3.right * obj, 1.5f));
     }
 
     private void M_PlaneRollData_XAngleUpdated(int obj)
     {
-        plane.localRotation = Quaternion.Euler(obj, 0, 0);
+        //plane.localRotation = Quaternion.Euler(obj, 0, 0);
+        StartCoroutine(RotateInTime(Vector3.forward * obj, 1.5f));
     }
 }
