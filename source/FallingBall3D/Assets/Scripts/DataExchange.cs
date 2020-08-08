96af89fc96582f1d57d9a3cb617f6251c20cb1e1
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2020-08-08 16:31:50 +0300

    Add external dll to Unity3D

--- source/FallingBall3D/Assets/Scripts/DataExchange.cs
+++ source/FallingBall3D/Assets/Scripts/DataExchange.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using DE = DataExchangeClient;
 
 public class DataExchange : MonoBehaviour
 {
+    [SerializeField]
+    private int m_Port;
+
+    [SerializeField]
+    private int m_BufferSize = 256;
+
+    private DE.DataExchangeClient client;
+
     void Start()
     {
-        
+        client = new DE.DataExchangeClient(m_Port, m_BufferSize);
+        client.Connect();
+
+        InvokeRepeating("GetData", 0f, 1f);
     }
 
-    void Update()
+    void GetData()
     {
-        
+        Debug.Log("Get data..");
+    }
+
+    void OnApplicationQuit()
+    {
+        client.Close();
     }
 }
