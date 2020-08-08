431780846ac6e9e17e8fd1f42cce02135e4b7782
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2020-08-08 16:19:59 +0300

    Add socket client library

---
+++ source/FallingBallServer/DataExchangeClient/DataExchangeClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace DataExchangeClient
+{
+    public class DataExchangeClient : SocketClient
+    {
+        protected override int bufferSize { get; set; }
+        public int Port { get; }
+
+        public DataExchangeClient(int port, int bufferSize = 256)
+        {
+            this.bufferSize = bufferSize;
+
+            Port = port;
+        }
+
+        public void Connect()
+        {
+            IPAddress ipAddr = IPAddress.Loopback;
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, Port);
+
+            Socket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Socket.Connect(ipEndPoint);
+        }
+    }
+}
---
+++ source/FallingBallServer/DataExchangeClient/SocketClient.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DataExchangeClient
+{
+    public abstract class SocketClient
+    {
+        protected abstract int bufferSize { get; set; }
+
+        public Socket Socket { get; protected set; }
+
+        public void Send(string response)
+        {
+            Socket.Send(Encoding.UTF8.GetBytes(response));
+        }
+
+        public string Receive()
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[bufferSize];
+                List<byte> data = new List<byte>();
+                int bytes, bytesTotal = 0;
+
+                do
+                {
+                    bytes = Socket.Receive(buffer);
+                    bytesTotal += bytes;
+                    data.AddRange(buffer.Take(bytes));
+                }
+                while (Socket.Available > 0);
+
+                return Encoding.UTF8.GetString(data.ToArray());
+            }
+        }
+
+        public void Close()
+        {
+            Socket.Shutdown(SocketShutdown.Both);
+            Socket.Close();
+        }
+    }
+}
