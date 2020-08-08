3f1dd880391af60ac06bdbd4681e55b0c8f41c2e
Andrey Kuritsyn <andrew.kuritsyn@gmail.com>
2020-08-08 17:15:13 +0300

    Add script to control ball with keyboard

---
+++ source/FallingBall3D/Assets/Scripts/BallController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var dx = Input.GetAxis("Horizontal"); 
+        var dz = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector3(dx, 0, dz) * 5, ForceMode.Force);
+    }
+}
